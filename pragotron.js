54333148c36e04e3797c53ba058e4804675e8749
kubakalisciak <jakub.kalisciak@gmail.com>
2024-06-13 15:51:15 +0200

    Add the aktualizujDane() function Add the aktualizujDane() function to update the data in the HTML. Also made so when opoznieniaI is empty, it says "on time".

--- pragotron.js
+++ pragotron.js
@@ -6,18 +6,36 @@ let numer;
 let opoznienie;
 
 function odczytajDane() {
-    const Koncowa = document.querySelector('#koncowa');
+    const Koncowa = document.querySelector('#koncowaI');
     koncowa = Koncowa.value;
-    const Posrednia = document.querySelector('#posrednia');
+    const Posrednia = document.querySelector('#posredniaI');
     posrednia = Posrednia.value;
-    const Godzina = document.querySelector('#godzina');
+    const Godzina = document.querySelector('#godzinaI');
     godzina = Godzina.value;
-        alert(godzina);
-    const Numer = document.querySelector('#numer');
+    const Numer = document.querySelector('#numerI');
     numer = Numer.value;
-    const Opoznienie = document.querySelector('#opoznienie');
-    opoznienie = Opoznienie.value;
+    const Opoznienie = document.querySelector('#opoznienieI');
+    if (Opoznienie.value == "") {
+        opoznienie = "Na czas";
+    }
+    else {
+        opoznienie = Opoznienie.value;
+    }
 }
 
-const odczytajP = document.querySelector('#odczytajP');
-odczytajP.onclick = () => odczytajDane();
+function aktualizujTablice() {
+    odczytajDane();
+    const Koncowa = document.querySelector('#koncowaT');
+    Koncowa.textContent = koncowa;
+    const Posrednia = document.querySelector('#posredniaT');
+    Posrednia.textContent = posrednia;
+    const Godzina = document.querySelector('#godzinaT');
+    Godzina.textContent = godzina;
+    const Numer = document.querySelector('#numerT');
+    Numer.textContent = numer;
+    const Opoznienie = document.querySelector('#opoznienieT');
+    Opoznienie.textContent = opoznienie;
+}
+
+const aktualizujP = document.querySelector('#aktualizujP');
+aktualizujP.onclick = () => aktualizujTablice();
